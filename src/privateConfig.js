421d8e620fe3bbf80ee3b8532dd152d6a539d87a
Andy Likuski <andy@likuski.org>
2018-07-27 14:31:45 -0700

    v1.2.33

--- src/privateConfig.js
+++ src/privateConfig.js
@@ -1,3 +1,9 @@
+
+/*
+  This represents private configuration values like keys and urls that are server specific.
+  You can copy the format of this and pass your version to createDefaultConfig. Or for
+  testing you can import this and merge it with specific values
+ */
 export default {
   // Settings is merged into the overall application state
   settings: {
